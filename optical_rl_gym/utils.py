a7bea80d4e0fb3dca677336706ef38368224ed56
Paul Cavallo <greendrone246@gmail.com>
2020-11-26 15:36:45 -0800

    Modulation addition to the PA environment

--- optical_rl_gym/utils.py
+++ optical_rl_gym/utils.py
@@ -17,7 +17,7 @@ class Service:
 
     def __init__(self, service_id, source, source_id, destination=None, destination_id=None, arrival_time=None,
                  holding_time=None, bit_rate=None, best_modulation=None, service_class=None, number_slots=None,
-                 launch_power=None):
+                 launch_power=None, modulation=None):
         self.service_id = service_id
         self.arrival_time = arrival_time
         self.holding_time = holding_time
@@ -33,6 +33,7 @@ class Service:
         self.initial_slot = None
         self.accepted = False
         self.launch_power = launch_power
+        self.modulation = modulation
 
     def __str__(self):
         msg = '{'
